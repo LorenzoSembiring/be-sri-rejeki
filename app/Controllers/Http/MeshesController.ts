7aa688c6f0d08ec092e73010deccb9e6741324cd
Lorenzo Laridho Sembiring <lorenzolyoko15@gmail.com>
2024-02-11 22:23:30 +0700

    Feat store mesh
    Implement store mesh with file upload

---
+++ app/Controllers/Http/MeshesController.ts
@@ -0,0 +1,52 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UsersController from './UsersController'
+import Mesh from 'App/Models/Mesh'
+
+export default class MeshesController {
+  public async store({ request, response, auth }: HttpContextContract) {
+    try {
+      const {name} = request.body()
+      const sanitized = name.replace(/ +/g, '-')
+      const usersController = new UsersController()
+      const user = await auth.authenticate()
+      const role = await usersController.getRole(user)
+
+      if (role == "admin") {
+
+        const file = request.file('file', {
+          size: '50mb',
+          extnames: ['glTF', 'obj', 'fbx']
+        })
+
+        await file?.moveToDisk('./mesh',{
+          name: sanitized + "." + file.extname,
+          overwrite: true
+        })
+        const fileName = file?.fileName
+
+        const mesh = await Mesh.create({
+          path: "mesh/" + fileName
+        })
+
+        return response.status(200).json({
+          code: 201,
+          status: "created",
+          message: "Mesh created successfully",
+          data: mesh
+        })
+      } else {
+        return response.status(401).json({
+          code: 401,
+          status: "Unauthorized",
+          message: "Your role access is not sufficient for this action"
+        })
+      }
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        status: "fail",
+        message: error
+      })
+    }
+  }
+}
