669ccd73dae58616e93dc1c227938e2d8c7a278a
Lorenzo Laridho Sembiring <lorenzolyoko15@gmail.com>
2024-02-12 23:39:27 +0700

    Fix delete destroy route

--- start/routes/texture.ts
+++ start/routes/texture.ts
@@ -4,6 +4,5 @@ Route.group(() => {
 
     Route.get('get/:id', 'TexturesController.get')
     Route.put('update/:id', 'TexturesController.update')
-    Route.delete('delete/:id', 'TexturesController.destroy')
 
 }).prefix('/api/product/texture')
