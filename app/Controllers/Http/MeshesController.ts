7cd91788444b7cb8eadd9382202364016f77bca3
Lorenzo Laridho Sembiring <lorenzolyoko15@gmail.com>
2024-02-12 12:43:18 +0700

    Feat update mesh
    Implement update function for mesh

--- app/Controllers/Http/MeshesController.ts
+++ app/Controllers/Http/MeshesController.ts
@@ -1,6 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import UsersController from './UsersController'
 import Mesh from 'App/Models/Mesh'
+import User from 'App/Models/User'
+import Drive from '@ioc:Adonis/Core/Drive'
 
 export default class MeshesController {
   public async store({ request, response, auth }: HttpContextContract) {
@@ -69,4 +71,59 @@ export default class MeshesController {
       })
     }
   }
+  public async update({ params, request, response, auth }: HttpContextContract) {
+    try {
+
+      const usersController: UsersController = new UsersController()
+      const user: User = await auth.authenticate()
+      const role: string = await usersController.getRole(user)
+      const {name} = request.body()
+      const sanitized = name.replace(/ +/g, '-')
+      const mesh: Mesh | null = await Mesh.find(params.id)
+      const oldpath: string | undefined = mesh?.path
+
+      if(role == "admin") {
+
+        const file = request.file('file', {
+          size: '50mb',
+          extnames: ['glTF', 'obj', 'fbx']
+        })
+
+        await file?.moveToDisk('./mesh',{
+          name: sanitized + "." + file.extname,
+          overwrite: true
+        })
+        const fileName = file?.fileName
+
+        if (fileName != undefined && mesh != null) {
+          mesh.name = name
+          mesh.path = "mesh/" + fileName
+          await mesh.save()
+        }
+        if (oldpath != undefined) {
+          await Drive.delete(oldpath)
+        }
+
+        return response.status(200).json({
+          code: 200,
+          status: "success",
+          message: "Mesh updated successfully"
+        })
+      } else {
+        return response.status(401).json({
+          code: 401,
+          status: "Unauthorized",
+          message: "Your role access is not sufficient for this action"
+        })
+      }
+
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        status: "fail",
+        message: error
+      })
+    }
+  }
+
 }
