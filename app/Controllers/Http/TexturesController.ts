8f24c393039b0aa2674e7869e6e61ff835b74498
Lorenzo Laridho Sembiring <lorenzolyoko15@gmail.com>
2024-02-12 22:35:48 +0700

    Feat update texture

---
+++ app/Controllers/Http/TexturesController.ts
@@ -0,0 +1,53 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UsersController from './UsersController'
+import User from 'App/Models/User'
+import Product from 'App/Models/Product'
+
+export default class TexturesController {
+  public async update ({ params, request, response, auth }: HttpContextContract) {
+    try {
+      const usersController: UsersController = new UsersController()
+      const user: User = await auth.authenticate()
+      const role = await usersController.getRole(user)
+
+      if(role == "admin"){
+        const product: Product | null = await Product.find(params.id)
+        const sanitized = product?.name.replace(/ +/g, '-')
+
+        const file = request.file('file', {
+          size: '10mb',
+          extnames: ['jpg', 'PNG', 'JPEG']
+        })
+
+        await file?.moveToDisk('./texture',{
+          name: sanitized + "." + file.extname,
+          overwrite: true
+        })
+        const fileName = file?.fileName
+
+        if (product != undefined && fileName != undefined) {
+          product!.texture = "texture/" + fileName!
+          await product.save()
+        }
+
+        return response.status(200).json({
+          code: 200,
+          status: "success",
+          message: "Texture updated successfully"
+        })
+      } else {
+        return response.status(401).json({
+          code: 401,
+          status: "Unauthorized",
+          message: "Your role access is not sufficient for this action"
+        })
+      }
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        status: "fail",
+        message: error
+      })
+    }
+  }
+}
