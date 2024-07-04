0abe655b5deb93ac7d1cc342229fe0833eb1ada1
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-04 19:04:17 +0700

    feat: best seller routing

--- start/routes/statistic.ts
+++ start/routes/statistic.ts
@@ -5,4 +5,5 @@ Route.group(() => {
   Route.get('product-count', 'StatisticsController.getProductCount')
   Route.get('order-count', 'StatisticsController.getOrderCount')
   Route.get('sales-sum', 'StatisticsController.getSalesSum')
+  Route.get('best-seller', 'StatisticsController.getBestSeller')
 }).prefix('/api/statistic')
