6e5692dbb896ec16f34784985f8da2a543918995
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-09 07:27:34 +0700

    feat: get stock by product

--- app/Controllers/Http/StocksController.ts
+++ app/Controllers/Http/StocksController.ts
@@ -1,8 +1,32 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import UsersController from './UsersController'
 import Size from 'App/Models/Size'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class StocksController {
+  public async getByProduct({ params, response }: HttpContextContract) {
+    try {
+      const productID = params.id
+
+      const stok = await await Database.rawQuery(
+        'select s.size, s.stock from sizes s WHERE s.product_id = :id ;',
+        {
+          id: productID,
+        }
+      )
+      return response.status(200).json({
+        code: 200,
+        message: 'success',
+        data: stok[0],
+      })
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        message: 'error',
+        error: error.message,
+      })
+    }
+  }
   public async update({ params, request, response, auth }: HttpContextContract) {
     const usersController = new UsersController()
     const { stok } = request.body()
@@ -14,35 +38,32 @@ export default class StocksController {
       const stock = await Size.findBy('id', params.id)
 
       if (role == 'admin' && stock) {
-
         stock.stock = stok
         await stock.save()
 
         return response.status(200).json({
           code: 200,
           status: 'success',
-          message: 'update success'
+          message: 'update success',
         })
-
-      } else if(role == 'admin' && !stock) {
+      } else if (role == 'admin' && !stock) {
         return response.status(404).json({
           code: 404,
           status: 'not found',
-          message: 'Size not found'
+          message: 'Size not found',
         })
-      }
-       else {
+      } else {
         return response.status(401).json({
           code: 401,
           status: 'Unauthorized',
-          message: 'Your role access is not sufficient for this action'
+          message: 'Your role access is not sufficient for this action',
         })
       }
     } catch (error) {
       return response.status(500).json({
         code: 500,
         status: 'fail',
-        message: error
+        message: error,
       })
     }
   }
@@ -55,5 +76,4 @@ export default class StocksController {
       return null
     }
   }
-
 }
--- start/routes/stock.ts
+++ start/routes/stock.ts
@@ -2,6 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
 
+  Route.get('/:id', 'StocksController.getByProduct')
   Route.put('update/:id', 'StocksController.update')
 
 }).prefix('/api/product/stock')
