26bbeacd7e360c443296aa118fd3e0a775575945
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-12 11:28:17 +0700

    feat: add route to userByAuth

--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -3,4 +3,5 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('authenticated-user', 'AuthController.userByAuth')
   Route.get('token-validation', 'AuthController.check')
+  Route.get('get', 'AuthController.userByAuth')
 }).prefix('/api/auth')
