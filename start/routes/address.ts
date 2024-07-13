c6dc119f1f7ac6b071660d4ac645dc20abc526c5
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-13 10:45:47 +0700

    feat: route address update selected

--- start/routes/address.ts
+++ start/routes/address.ts
@@ -5,6 +5,7 @@ Route.group(() => {
     Route.post('store', 'AddressesController.store')
     Route.get('get/:id', 'AddressesController.get')
     Route.put('update/:id', 'AddressesController.update')
+    Route.put('update-selected/:id', 'AddressesController.selectAddress')
     Route.delete('delete/:id', 'AddressesController.destroy')
 
 }).prefix('/api/address')
