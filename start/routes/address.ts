e66cdad6b1a7273e775aee01a824ade2164adbad
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-13 22:01:03 +0700

    fix: remove id from endpoint

--- start/routes/address.ts
+++ start/routes/address.ts
@@ -3,7 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
 
     Route.post('store', 'AddressesController.store')
-    Route.get('get/:id', 'AddressesController.get')
+    Route.get('get', 'AddressesController.get')
     Route.put('update/:id', 'AddressesController.update')
     Route.put('update-selected/:id', 'AddressesController.selectAddress')
     Route.delete('delete/:id', 'AddressesController.destroy')
