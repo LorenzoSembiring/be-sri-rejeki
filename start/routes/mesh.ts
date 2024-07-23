23fd1958ac02fb390f8783e881cc51a80ba4df21
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-23 08:36:07 +0700

    feat: route index and get product 3d by id

--- start/routes/mesh.ts
+++ start/routes/mesh.ts
@@ -3,7 +3,9 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
 
     Route.post('store', 'MeshesController.store')
+    Route.get('index', 'MeshesController.index')
     Route.get('get/:id', 'MeshesController.get')
+    Route.get('get-product/:id', 'MeshesController.get3D')
     Route.put('update/:id', 'MeshesController.update')
     Route.delete('delete/:id', 'MeshesController.destroy')
 
