6877863c93ff89950eb09280ddc47ada0178ef6e
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-23 08:36:27 +0700

    feat: bulk update order route

--- start/routes/order.ts
+++ start/routes/order.ts
@@ -3,7 +3,11 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
 
     Route.post('store', 'OrdersController.store')
+    Route.post('placing-order', 'OrdersController.placingOrder')
+    Route.post('check-shipping', 'OrdersController.checkShipping')
     Route.get('get/:id', 'OrdersController.get')
+    Route.get('midtrans-status/:id', 'OrdersController.midtransStatus')
+    Route.get('get-province', 'OrdersController.getProvince')
     Route.put('update/:id', 'OrdersController.update')
     Route.delete('delete/:id', 'OrdersController.destroy')
 
